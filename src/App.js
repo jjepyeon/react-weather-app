16353e2ac9a027f530a093b09ef6fbb27289babc
jjepyeon <66534630+jjepyeon@users.noreply.github.com>
2022-05-31 20:00:05 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Jane Pyeon and is open sourced on{" "}
+        <a href="https://github.com/jjepyeon/react-weather-app" target="blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
